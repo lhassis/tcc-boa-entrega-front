b6fdd1b60333f65e275be6609041ddf031be19ba
Lucas Assis <assis.lucash@gmail.com>
2021-10-17 19:20:57 -0300

    ajuste de responsividade na tela de login

--- src/views/login.js
+++ src/views/login.js
@@ -42,7 +42,8 @@ class Login extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                <div className="col-md-3"></div>
+                <div className="col-sm-12 col-md-6" >
                     <div className="bs-docs-section">
                         <Card title="Login">
                             <div className="row">
@@ -80,6 +81,7 @@ class Login extends React.Component {
 
                     </div>
                 </div>
+                <div className="col-md-3"></div>
             </div>
         )
     }
